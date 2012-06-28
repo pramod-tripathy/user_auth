f3cc16ff3b00b6fe6c92bea263ed775828422cad
pramod <pramod.tripathy@gmail.com>
2012-06-28 12:59:40 +0530

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+
+class UsersController < ApplicationController
+  before_filter :save_login_state, :only => [:new, :create]
+  def new
+    @user = User.new
+  end
+  	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:notice] = "You Signed up successfully"
+			flash[:color]= "valid"
+		else
+			flash[:notice] = "Form is invalid"
+			flash[:color]= "invalid"
+		end
+		render "new"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserAuth::Application.config.secret_token = 'b2d840c614e064cf635dfe883693e64ff3ff5064cc6f7f0032c323e7b0b5c228ab0a5ad6a13b67552b8396c20a89dc1b212f4f145476cfb7c505510a43aad8b9'
